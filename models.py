bc725c4be5c63f5ada601ebc6439e418a0dd5345
Marco Richetta <marcorichetta@gmail.com>
2023-05-21 04:05:41 -0300

    remove unneeded fields

--- models.py
+++ models.py
@@ -1,37 +1,10 @@
 from pydantic import BaseModel
-from typing import Optional
 
 
 class GithubUserModel(BaseModel):
-    login: str
-    id: int
-    node_id: str
-    avatar_url: str
-    gravatar_id: str
-    url: str
-    html_url: str
-    followers_url: str
-    following_url: str
-    gists_url: str
-    starred_url: str
-    subscriptions_url: str
-    organizations_url: str
-    repos_url: str
-    events_url: str
-    received_events_url: str
-    type: str
-    site_admin: bool
-    name: Optional[str]
-    company: Optional[str]
+    name: str | None
     blog: str
-    location: Optional[str]
-    email: Optional[str]
-    hireable: Optional[bool]
-    bio: Optional[str]
-    twitter_username: Optional[str]
+    bio: str | None
     public_repos: int
-    public_gists: int
     followers: int
-    following: int
-    created_at: str
-    updated_at: str
+    avatar_url: str
